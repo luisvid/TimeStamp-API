56760ad38667fce181e63c0d100b8eb7d8eebb25
Luis Videla <luisvid@gmail.com>
2021-06-28 10:01:37 -0300

    timestampimg dir

--- src/application.ts
+++ src/application.ts
@@ -98,7 +98,7 @@ export class AuthApplication extends BootMixin(
    */
   protected configureFileUpload(destination?: string) {
     // Upload files to `dist/.sandbox` by default
-    destination = destination ?? path.join(__dirname, '../../.timestampimg');
+    destination = destination ?? path.join(__dirname, '../.timestampimg');
     this.bind(STORAGE_DIRECTORY).to(destination);
     const multerOptions: multer.Options = {
       storage: multer.diskStorage({
