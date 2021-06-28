fc6ad771ee5d31f615a6a70a040bca701bbdb0de
Luis Videla <luisvid@gmail.com>
2021-06-28 15:49:41 -0300

    verify bfa moved to get /files

--- src/controllers/storage.controller.ts
+++ src/controllers/storage.controller.ts
@@ -83,14 +83,6 @@ export class StorageController {
     let arrLocations: Array<string> = [];
     let hashesID: {key: string, value: string}[] = [];
     let today = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    let limiteVueltas = 15; // Establecemos un límite de la máxima cantidad de veces que se hará el do-while
-    let acumuladorVueltas = 0; // Variable de corte de do-while
-    let finishDoWhile = false;
-
-    console.log(currentUser);
-    console.log("Nombre Archivo: " + fileName);
-    console.log("Descripción Archivo: " + fileDescription);
-
 
     // TODO
     // 1- Obtener HASH
@@ -109,14 +101,15 @@ export class StorageController {
       upload.any()(request, response, async (err: any) => {
         if (err) reject(err)
         else {
-          let res = new Array()
+          let res = new Array();
+          let cant = (request as any).files.length;
           let cont = 1;
           for (const file of (request as any).files) {
             // 1 - Get HASH FILE
             fileHash = getHashFromFile(file);
             allHashes.push(fileHash)
             fileName = request.query.name ?? file.originalname;
-            fileName = fileName + cont; //+ file.originalname.slice(-4);
+            fileName = fileName + ((cant > 1) ? ' - ' + cont : '');
             cont++;
 
             const params = {
@@ -144,7 +137,6 @@ export class StorageController {
                   @p_id_usuario = "${currentUser.id}",
                   @p_descripcion = "${fileDescription}"`;
             try {
-              console.log(sp);
               const spInsertRetVal = await this.usuarioRepository.dataSource.execute(sp);
               hashesID.push({key: fileHash, value: spInsertRetVal[0].id});
             } catch (err) {
@@ -156,7 +148,6 @@ export class StorageController {
         }
       })
     })
-    console.log('Upload S3 y DB Insert OK');
 
     // 4 - Call endpoints nodo sellador
     let stampHashes: MyStampHashes = {
@@ -167,71 +158,15 @@ export class StorageController {
     console.log('hashes stampados');
     console.log(txRetVal);
 
-
-    // ESPERAR 10 SEGUNDOS PARA DAR TIEMPO A QUE LAS TRANSACCIONES ENVIADAS SEAN INCLUIDAS EN UN BLOQUE
-    console.log("espera 10 segundos");
-    await new Promise(f => setTimeout(f, 15000));
-
-    // Por cada hash enviado
-    for (var i = 0; i < allHashes.length; i++) {
-      do {
-        console.log("veriifica hash: " + allHashes[i])
-        // invoca http://nodocolmed.greykoda.com:3000/verify/HASH
-        const txVerify = await this.stampApiService.getVerify(allHashes[i]);
-
-        console.log('verificación ');
-        console.log(txVerify.stamped);
-
-        // 5- SP Update DB
-        // Revisar objeto data para ver el status
-        if (txVerify.stamped) {
-          finishDoWhile = true;
-          // updates DB
-          sp = `exec dbo.sp_bfa_documento_update
-                @p_id_documento = "${hashesID[i].value}",
-                @p_bfa_block_number = "${txVerify.stamps[0].blocknumber}",
-                @p_bfa_block_timestamp = "${txVerify.stamps[0].blocktimestamp}",
-                @p_bfa_who_stamped = "${txVerify.stamps[0].whostamped}"`;
-
-          const spUpdateRetVal = await this.usuarioRepository.dataSource.execute(sp);
-          console.log(spUpdateRetVal);
-        } else {
-          acumuladorVueltas++;
-        }
-
-        // ESPERAR 1 SEGUNDO PARA NO LLAMAR TANTAS VECES AL ENDPOINT DE VERIFICACION
-        console.log("esperoa de 5 segundos antes de reintentar");
-        await new Promise(f => setTimeout(f, 5000));
-
-        if (acumuladorVueltas > limiteVueltas) {
-          finishDoWhile = true;
-        }
-
-      } while (!finishDoWhile);
-    }; // end for
-
-    console.log('Verify  OK');
+    console.log('Upload S3, DB Insert & Stamp OK');
 
     // Retorno parcial al fron-ent ->
     const strRet = 'DOCUMENTO UPLOAD OK, DOC SAVE DB OK, DOC STAMPED OK'
     arrLocations.push(strRet);
 
     return Promise.resolve(arrLocations);
+
   }
-
-
-
-  // endpoint de búsqueda por archivo
-  // todos los docs del usuario usuarioID (obtenido de TOKEN)
-  // A futuro permitir filtros, límites, fechas
-  // LA SP hace internamente el like
-  /*ALTER Procedure [dbo].[sp_bfa_documento_select](
-    @p_bfa_block_number varchar(100) = 0, // Si mandamos 0, es xq queremos los que estan sin estampar, sin importar usuario
-    @p_id_documento int = Null,
-    @p_n_documento varchar(200) = Null,
-    @p_id_usuario int = NUll, // Tomar desde el token
-    @p_descripcion varchar(400) = Null
-    )*/
 
   @authenticate("jwt")
   @get('/files/{fileName}', {
@@ -253,20 +188,46 @@ export class StorageController {
     @param.path.string('fileName') fileName: string,
   ): Promise<any[]> {
 
-    let documentFind: any[];
+    let arrDocuments: any[];
 
     // ejecución de SP
     let sp = `exec dbo.sp_bfa_documento_select
       @p_n_documento = "${fileName}",
       @p_id_usuario = "${currentUser.id}"`;
 
-    documentFind = await this.usuarioRepository.dataSource.execute(sp);
+    arrDocuments = await this.usuarioRepository.dataSource.execute(sp);
 
-    for (var i = 0; i < documentFind.length; i++) {
-      documentFind[i].n_adjunto = baseURL + documentFind[i].n_adjunto.slice(-69);
+    // recorre array de documentos
+    for (var i = 0; i < arrDocuments.length; i++) {
+      // armo url completa del documento en S3
+      arrDocuments[i].n_adjunto = baseURL + arrDocuments[i].n_adjunto.slice(-69);
+
+      // si no fue verificado en la bfa
+      if (arrDocuments[i].bfa_block_number == "") {
+        //Verificar doc sin blocknumer
+        console.log("verifica hash: " + arrDocuments[i].bfa_hash)
+        // invoca http://nodocolmed.greykoda.com:3000/verify/HASH
+        const txVerify = await this.stampApiService.getVerify(arrDocuments[i].bfa_hash);
+
+        // si se verificó correctamente
+        if (txVerify.stamped) {
+          arrDocuments[i].bfa_block_number = txVerify.stamps[0].blocknumber;
+          // 5- SP Update DB
+          sp = `exec dbo.sp_bfa_documento_update
+            @p_id_documento = "${arrDocuments[i].id_documento}",
+            @p_bfa_block_number = "${txVerify.stamps[0].blocknumber}",
+            @p_bfa_block_timestamp = "${txVerify.stamps[0].blocktimestamp}",
+            @p_bfa_who_stamped = "${txVerify.stamps[0].whostamped}"`;
+
+          const spUpdateRetVal = await this.usuarioRepository.dataSource.execute(sp);
+        }
+      }
     }
 
-    return Promise.resolve(documentFind);
+    console.log('SEARCH DOCS, VERYFY & DB UPDATE OK');
+    return Promise.resolve(arrDocuments);
   }
 
 }
+
+
